c712e7cb0f808524a640b9fcaa2f46fb2b9d7329
Savannah Greuel <smallidgesavannah@gmail.com>
2019-04-05 09:45:15 -0600

    ring buffer complete

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,14 +1,17 @@
 class RingBuffer:
   def __init__(self, capacity):
     self.capacity = capacity
-    self.current = 0
-    self.storage = [None]*capacity
+    self.current = 0 #current index to overwrite
+    self.storage = [None]*capacity 
 
   def append(self, item):
     #new item, gets put in oldest datapoint's spot. ( index 0)
-    self.storage.pop(0)
-    self.storage.append(item)
+    self.storage[self.current] = item 
+    if self.current < self.capacity - 1:
+      self.current+=1
+    else:
+      self.current = 0
 
 
   def get(self):
-    return self.storage
+    return [item for item in self.storage if item is not None]
